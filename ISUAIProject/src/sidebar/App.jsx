46ef7dc7c0f24aa74dd79fee6194f973cc06b3e3
Truman-Folkers <trumanjackm@gmail.com>
2025-12-04 15:07:02 -0600

    bro

--- ISUAIProject/src/sidebar/App.jsx
+++ ISUAIProject/src/sidebar/App.jsx
@@ -1,10 +1,7 @@
+import Sidebar from "sidebar.jsx"
+
 export default function App() {
   return (
-    <div style={{ padding: 16 }}>
-      <h2>CyAI</h2>
-
-      
-      <button>Ask AI</button>
-    </div>
+    <Sidebar />
   );
 }
